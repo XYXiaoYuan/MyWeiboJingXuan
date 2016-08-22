92832eb5254521638d18a001e5c906250db1cb8e
yuanxiaorong <664995775@qq.com>
2016-08-22 19:08:12 +0800

    模块属性映射问题

---
+++ MyWeiboJingXuan/MyWeiboJingXuan/Classes/Other/XYModelConfig.h
@@ -0,0 +1,13 @@
+//
+//  XYModelConfig.h
+//  bai
+//
+//  Created by yuan on 15/11/20.
+//  Copyright © 2015年 袁小荣. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYModelConfig : NSObject
+
+@end
